3afbf0bc01cbba98e6fe6ed967b5f381c3aeb3b2
Rahul D Bhalekar <91216950+bhalekarr@users.noreply.github.com>
2022-07-01 20:51:57 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ShopBridge.Data;
+using ShopBridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridge
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(
+                config => 
+                { 
+                    config.User.RequireUniqueEmail = true;
+                    config.SignIn.RequireConfirmedEmail = false;
+                    config.SignIn.RequireConfirmedPhoneNumber = false;
+                }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<IProductRepository, SqlProductRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //use broswerlink
+                app.UseBrowserLink();
+
+                app.UseDeveloperExceptionPage();
+
+                //use database error page
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                //use exception handler
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            //for Bootstraps, JQuery, JS, others
+            app.UseStaticFiles();
+
+            //Register and login
+            app.UseAuthentication();
+
+            app.UseHttpsRedirection();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                //or for api route use launchsettings.json and add
+                //"launchUrl": "api/values"
+            });
+        }
+    }
+}
