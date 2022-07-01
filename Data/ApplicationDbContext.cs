c87a6ea9e4c8d17f46a4913d59037ff8ce11eb01
Rahul D Bhalekar <91216950+bhalekarr@users.noreply.github.com>
2022-07-01 21:03:31 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopBridge.Models;
+
+namespace ShopBridge.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
